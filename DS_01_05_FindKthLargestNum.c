e455fbab8b301ed52346a8c618c77958b0a46273
LuoYY <laoluo1991@gmail.com>
2016-03-20 12:46:11 +0800

    Find Kth Largest Num in the unsorted set

---
+++ DS_01_05_FindKthLargestNum.c
@@ -0,0 +1,63 @@
+/*
+* Data Structures in C
+* Find Kth Largest Num in the unsorted set 
+* 20160320
+* LuoYingYing
+*/
+
+
+#include <stdio.h>
+
+int FindKthLargest(int S[], int K, int Low, int High);
+void Swap(int *a, int *b);
+
+int main(int argc, char const *argv[])
+{
+	int S[6] = {4,8,6,9,2,5};
+	printf("%d\n", FindKthLargest(S, 2, 0, 5));
+	return 0;
+}
+
+int FindKthLargest(int S[], int K, int Low, int High)
+{
+ 	int Pivot = S[Low];
+	int Left = Low, Right = High;
+	if (Low >= High)
+	{
+		return Pivot;
+	}
+	Swap(&S[Low], &S[High]);
+	while(1)
+	{
+		while(Low < High && S[Low] >= Pivot) Low++;
+		while(Low < High && S[High] <= Pivot) High--;
+		if (Low < High)
+		{
+			Swap(&S[Low], &S[High]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	Swap(&S[Low], &S[Right]);
+	if (K <= High - Left)
+	{
+		return FindKthLargest(S, K, Left, High-1);
+	}
+	else if(K == High - Left + 1)
+	{
+		return Pivot;
+	}
+	else
+	{
+		return FindKthLargest(S, K - High + Left - 1, High + 1, Right);
+	}
+}
+
+void Swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
